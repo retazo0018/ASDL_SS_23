b45a1062a76693428b91a738c38b3e82ce8ec5b6
retazo0018 <ashwin.cse18@gmail.com>
2023-07-20 09:48:03 +0200

    Added ReadMe and shell script for easy execution.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# ASDL_SS_23
+# Project work for the course "Analysing Software using Deep Learning" at the University of Stuttgart
+
+## The code found here was modified from the repository: https://github.com/michiyasunaga/BIFI
+
+### Please run `./src/run-PyPI-FixerOnly.sh` from the base directory.
--- code/src/run-PyPI-FixerOnly.sh
+++ code/src/run-PyPI-FixerOnly.sh
@@ -4,10 +4,18 @@
 ################################################################################
 # export PYTHONPATH=.
 
+#Prepare Data
 python -m utils.get_pypibugs "./data/pypibugs/pypi-bugs.jsonl"
 python scripts/split_orig_bad_and_good.py
 python scripts/generate_paired_data_r0.py
 
-python -m src.c001__train_fixer --round_name round0 --max_epoch 20 --continue_from "./data/round0/model-fixer/checkpoint.pt"
-python -m src.c003__run_fixer   --round_name round0 --checkpoint_name "checkpoint20.pt"
+#Round0
+python -m src.c001__train_fixer --round_name round0 --max_epoch 5 --continue_from "./data/round0/model-fixer/checkpoint.pt"
+python -m src.c003__run_fixer   --round_name round0 --checkpoint_name "checkpoint_best.pt"
 python -m src.c005__eval_fixer  --round_name round0
+
+#Round1
+python -m src.c006__generate_paired_data_from_fixer --round_name round0 --out_round_name round1-FixerOnly
+python -m src.c001__train_fixer --round_name round1-FixerOnly --max_epoch 1 --continue_from 'data/round0/model-fixer/checkpoint_best.pt'
+python -m src.c003__run_fixer   --round_name round1-FixerOnly --checkpoint_name "checkpoint_best.pt"
+python -m src.c005__eval_fixer --round_name round1-FixerOnly
