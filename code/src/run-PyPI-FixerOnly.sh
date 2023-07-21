2600fa70e095c1d39c4288d92a95fb280d8e8672
retazo0018 <ashwin.cse18@gmail.com>
2023-07-21 09:43:07 +0200

    Update shell script

--- code/src/run-PyPI-FixerOnly.sh
+++ code/src/run-PyPI-FixerOnly.sh
@@ -10,7 +10,7 @@ python scripts/split_orig_bad_and_good.py
 python scripts/generate_paired_data_r0.py
 
 #Round0
-python -m src.c001__train_fixer --round_name round0 --max_epoch 5 --continue_from "./data/round0/model-fixer/checkpoint.pt"
+python -m src.c001__train_fixer --round_name round0 --max_epoch 3 --continue_from "./data/round0/model-fixer/checkpoint_vb.pt"
 python -m src.c003__run_fixer   --round_name round0 --checkpoint_name "checkpoint_best.pt"
 python -m src.c005__eval_fixer  --round_name round0
 
